f61eb56539bc1e7e8ca261f59219888d9bfc18c9
MarceloCardoso <marceloguimaraes780@gmail.com>
2023-07-19 10:03:20 -0300

    att

---
+++ Aulas/12 string/script.js
@@ -0,0 +1,19 @@
+let umaString = "O rato roeu a roupa do rei de roma.";
+
+console.log(umaString.concat(' em um lindo dia'));
+
+console.log(`${umaString} em um lindo dia`)
+
+console.log(umaString + ' em um lindo dia')
+
+console.log(umaString.indexOf('o', 3));
+
+console.log(umaString.lastIndexOf('m', 3));
+
+console.log(umaString.replace(/r/g, '#'));
+
+console.log(umaString.length);
+
+console.log(umaString.slice(-5, -1));
+
+console.log(umaString.split(' ', 3));
---
+++ Aulas/15 Math/script.js
@@ -0,0 +1,24 @@
+console.log(100/0);
+
+
+
+// let num1 = 9
+// console.log(num1 ** 0.5);
+
+
+// let num2 = Math.floor(num1); // arredonda pra cima
+// let num2 = Math.ceil(num1); // arredonda pra baixo
+// let num2 = Math.round(num1); // arredonda conforme decimal
+
+// console.log(Math.max(1,2,3,4,5,6,7,8,9,10));
+
+// console.log(Math.min(-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10));
+
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
+// console.log(aleatorio);
+
+// console.log(Math.PI);
+
+// console.log(Math.pow(2, 10));
+// console.log(2 ** 10);
